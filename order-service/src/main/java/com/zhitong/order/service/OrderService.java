ce0b12e780158e2f9aab9e742e82959d4e6576c7
willshang76 <willshang76@gmail.com>
2024-04-03 00:05:49 -0700

    Distributed lock solution to handle concurrency.

--- order-service/src/main/java/com/zhitong/order/service/OrderService.java
+++ order-service/src/main/java/com/zhitong/order/service/OrderService.java
@@ -8,6 +8,8 @@ import com.zhitong.internalcommon.datatoobject.ResponseResult;
 import com.zhitong.internalcommon.response.GetPriceResponse;
 import com.zhitong.order.entity.Order;
 import com.zhitong.order.repository.OrderRep;
+import lombok.var;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,10 +19,12 @@ import java.util.Optional;
 public class OrderService {
     private final PriceService priceService;
     private final OrderRep orderRep;
+    private final RedissonClient redissonClient;
 
-    public OrderService(PriceService priceService, OrderRep orderRep) {
+    public OrderService(PriceService priceService, OrderRep orderRep, RedissonClient redissonClient) {
         this.priceService = priceService;
         this.orderRep = orderRep;
+        this.redissonClient = redissonClient;
     }
 
     public ResponseResult<?> initializeOrder(String phoneNumber, Location souLocation, Location desLocation, String vehicleType) {
@@ -46,24 +50,53 @@ public class OrderService {
 
     public synchronized ResponseResult<?> assignOrder(int orderId, int vehicleId) {
 
-        List<Order> existingOrderList = this.orderRep.findByStatusAndVehicleId(OrderStatus.ASSIGNED, vehicleId);
-        if (!existingOrderList.isEmpty()) {
-            return ResponseResult.fail(ResponseStatus.EXISTING_ORDER.getCode(), ResponseStatus.EXISTING_ORDER.getMessage());
+        var vehicleLock = redissonClient.getLock("vehicleId:" + vehicleId);
+        var orderLock = redissonClient.getLock("orderId:" + orderId);
+
+        try {
+            // Acquire the lock
+            boolean isVehicleLocked = vehicleLock.tryLock();
+
+            if (isVehicleLocked) {
+
+                List<Order> existingOrderList = this.orderRep.findByStatusAndVehicleId(OrderStatus.ASSIGNED, vehicleId);
+                if (!existingOrderList.isEmpty()) {
+                    return ResponseResult.fail(ResponseStatus.EXISTING_ORDER.getCode(), ResponseStatus.EXISTING_ORDER.getMessage());
+                }
+                // Acquire the lock
+                boolean isOrderLocked = orderLock.tryLock();
+
+                if (isOrderLocked) {
+
+                    Optional<Order> optionalPendingOrder = this.orderRep.findById(orderId);
+
+                    if (!optionalPendingOrder.isPresent() || optionalPendingOrder.get().getStatus() != OrderStatus.PENDING) {
+                        return ResponseResult.fail(ResponseStatus.ORDER_NOT_FOUND.getCode(), ResponseStatus.ORDER_NOT_FOUND.getMessage());
+                    }
+
+                    Order pendingOrder = optionalPendingOrder.get();
+                    pendingOrder.setVehicleId(vehicleId);
+                    pendingOrder.setStatus(OrderStatus.ASSIGNED);
+
+                    orderRep.save(pendingOrder);
+
+                    System.out.println("pendingOrder = " + pendingOrder);
+
+                    return ResponseResult.success(pendingOrder);
+                } else {
+                    System.out.println("Could not acquire the order lock");
+                    return ResponseResult.fail(ResponseStatus.ORDER_NOT_FOUND.getCode(), ResponseStatus.ORDER_NOT_FOUND.getMessage());
+                }
+
+
+            } else {
+                System.out.println("Could not acquire the vehicle lock");
+                return ResponseResult.fail(ResponseStatus.ORDER_NOT_FOUND.getCode(), ResponseStatus.ORDER_NOT_FOUND.getMessage());
+            }
+        } finally {
+            // Ensure the lock is released even if an exception occurs
+            vehicleLock.unlock();
+            orderLock.unlock();
         }
-        Optional<Order> optionalPendingOrder = this.orderRep.findById(orderId);
-
-        if (!optionalPendingOrder.isPresent() || optionalPendingOrder.get().getStatus() != OrderStatus.PENDING) {
-            return ResponseResult.fail(ResponseStatus.ORDER_NOT_FOUND.getCode(), ResponseStatus.ORDER_NOT_FOUND.getMessage());
-        }
-
-        Order pendingOrder = optionalPendingOrder.get();
-        pendingOrder.setVehicleId(vehicleId);
-        pendingOrder.setStatus(OrderStatus.ASSIGNED);
-
-        orderRep.save(pendingOrder);
-
-        System.out.println("pendingOrder = " + pendingOrder);
-
-        return ResponseResult.success(pendingOrder);
     }
 }
